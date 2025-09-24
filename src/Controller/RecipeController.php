5f79c571f7b8b9c6dad34e8c2b9fc9e2ca69101d
Abdellah <abdallahkaba98@gmail.com>
2025-09-24 20:00:51 +0000

    suite de controller

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,15 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RecipeController extends AbstractController
 {
-    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'] )]
-    public function index(Request $request, $slug, $id): Response
+    #[Route('/recette', name: 'recipe.index')]
+    public function index(Request $request): Response
     {
-        dd($slug, $id);
+       return new Response("Recette");
+    }
+
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'] )]
+    public function show(Request $request, $slug, $id): Response
+    {
+        return $this->json([
+            'id' => $id,
+            'slug' => $slug
+        ]);
+
+        // return new Response("Recette $id - $slug");
+        // return new JsonResponse([
+        //     'id' => $id,
+        //     'slug' => $slug
+        // ]);
     }
 }
