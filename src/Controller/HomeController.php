cd09b274f2f04b6007624eab9457005f3007fced
Abdellah <abdallahkaba98@gmail.com>
2025-09-24 19:43:49 +0000

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController {
+
+    #[Route('/', name: 'home')]
+
+    function index (Request $request): Response {
+
+        dd($request);
+        return new Response('Hello, Symfony!' . $request->query->get('name', 'Inconnu'));
+
+    }
+}
